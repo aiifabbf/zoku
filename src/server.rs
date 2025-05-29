438dab4aca54792bc1224daa1eef2a20c533af76
Benjamin Shi <aiifabbf@outlook.com>
2025-05-29 14:29:45 +0800

    Dedicated worker to write into master pty; use bounded channel to limit memory growth, deadlock condition yet to verify.

--- src/server.rs
+++ src/server.rs
@@ -15,10 +15,10 @@ use tokio::{
     net::UnixListener,
     runtime::Runtime,
     select, spawn,
-    sync::mpsc::unbounded_channel,
+    sync::mpsc::{channel, unbounded_channel},
 };
 
-const BUFFER_SIZE: usize = 1000; // bytes
+const BUFFER_SIZE: usize = 4096; // bytes
 const REPLAY_SIZE: usize = 1_000_000; // bytes
 
 pub fn main(bind: &Path, argv: &[CString]) {
@@ -27,14 +27,14 @@ pub fn main(bind: &Path, argv: &[CString]) {
             let rt = Runtime::new().unwrap();
             rt.block_on(async {
                 let (new_client_sender, mut new_client_receiver) = unbounded_channel();
-                let (from_client_sender, mut from_client_receiver) = unbounded_channel::<Vec<u8>>();
+                let (from_client_sender, mut from_client_receiver) = channel::<Vec<u8>>(BUFFER_SIZE);
                 let listener = UnixListener::bind(bind).expect("address already in use");
 
                 let to_master_sender = from_client_sender.clone();
                 let _listener_worker = spawn(async move {
                     while let Ok((mut client, _addr)) = listener.accept().await {
                         let (from_master_sender, mut from_master_receiver) =
-                            unbounded_channel::<Vec<u8>>();
+                            channel::<Vec<u8>>(BUFFER_SIZE);
                         // dbg!("sending channels to master");
                         new_client_sender.send(from_master_sender).unwrap();
                         let to_master_sender = to_master_sender.clone();
@@ -47,7 +47,7 @@ pub fn main(bind: &Path, argv: &[CString]) {
                                             break;
                                         }
                                         // dbg!("client worker: sending to master {}", from_utf8(&buffer));
-                                        to_master_sender.send(buffer[..n].to_owned()).unwrap();
+                                        to_master_sender.send(buffer[..n].to_owned()).await.unwrap();
                                     }
                                     Some(delta) = from_master_receiver.recv() => {
                                         // dbg!("client worker: writing to client {}", from_utf8(&delta));
@@ -67,22 +67,25 @@ pub fn main(bind: &Path, argv: &[CString]) {
                     unsafe { File::from_raw_fd(master.try_clone().unwrap().into_raw_fd()) };
                 let mut write = unsafe { File::from_raw_fd(master.into_raw_fd()) };
 
+                let _master_worker = spawn(async move {
+                    while let Some(delta) = from_client_receiver.recv().await {
+                        // dbg!("master worker: writing to process {}", from_utf8(&delta));
+                        write.write_all(&delta).await.ok()?;
+                        write.flush().await.ok()?;
+                    }
+                    Some(())
+                });
+
                 loop {
                     let mut buffer = [0; BUFFER_SIZE];
                     select! {
                         biased;
-                        Some(delta) = from_client_receiver.recv() => {
-                            // dbg!("master worker: writing to process {}", from_utf8(&delta));
-                            write.write_all(&delta).await.unwrap();
-                            write.flush().await.unwrap();
-                            // dbg!("master worker: writing to process done");
-                        }
                         Some(to_new_client_sender) =
                             new_client_receiver.recv() => {
                                 // dbg!("master worker: new client");
                                 // dbg!("master worker: sending replay to client");
-                                to_new_client_sender.send(replay.as_slices().0.to_owned()).unwrap();
-                                to_new_client_sender.send(replay.as_slices().1.to_owned()).unwrap();
+                                to_new_client_sender.send(replay.as_slices().0.to_owned()).await.unwrap();
+                                to_new_client_sender.send(replay.as_slices().1.to_owned()).await.unwrap();
                                 clients.push(to_new_client_sender);
                                 // dbg!("master worker: replay sent");
                             }
@@ -102,10 +105,16 @@ pub fn main(bind: &Path, argv: &[CString]) {
                                     break;
                                 }
                             }
-                            clients.retain(|to_client_sender| {
+
+                            let mut active_clients = vec![];
+
+                            for to_client_sender in clients.into_iter() {
                                 // dbg!("master worker: sending delta to client");
-                                to_client_sender.send(buffer[..n].to_owned()).is_ok()
-                            });
+                                if to_client_sender.send(buffer[..n].to_owned()).await.is_ok() {
+                                    active_clients.push(to_client_sender);
+                                }
+                            }
+                            clients = active_clients;
                         }
                         else => break
                     }
